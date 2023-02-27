6be197524e82fd7cf15dd56fd959763fb3bc85c0
XingyuLi20 <lixingyu@berkeley.edu>
2023-02-27 13:46:52 -0700

    add: dotenv

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config();
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_CHROME_EXTENSION_ID:
+      process.env.NEXT_PUBLIC_CHROME_EXTENSION_ID,
+    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    CENTER_API_KEY: process.env.CENTER_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
