f1a365e94d07d87f5d860486d80de094c853868c
XingyuLi20 <lixingyu@berkeley.edu>
2023-03-12 20:13:45 -0700

    fix: footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p className="text-gray-500 text-center m-8 md:m-20 lg:m-20 xl:m-20">
-        © 2023 Eth Denver. All rights reserved.{" "}
+        © 2023 Sofamon. All rights reserved.{" "}
         <a href="https://twitter.com/sofamon_xyz" target="_blank">
           <u>Twitter</u>
         </a>
